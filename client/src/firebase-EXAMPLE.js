677b6b4b1c648637deb6e5d472b6aaf16b9ecc43
Reyhan Huditama <reyhan21098@gmail.com>
2019-04-23 09:29:30 +0700

    added firebase-EXAMPLE.js config file

---
+++ client/src/firebase-EXAMPLE.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+const config = {
+    apiKey: "your credentials here...",
+    authDomain: "your credentials here...",
+    databaseURL: "your credentials here...",
+    projectId: "your credentials here...",
+    storageBucket: "your credentials here...",
+    messagingSenderId: "your credentials here..."
+};
+
+export default config
